6a2df274570a922269121f6c51363745428f9492
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-08-12 11:01:23 -0700

    Made some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+u = User.create(
+  name: "Joseph Tibbertsma",
+  email: "email@email.com",
+  password: "password123"
+)
+
+15.times do
+  description = 5.times.inject([]) do |memo, _|
+    memo << Faker::Lorem.paragraph(10)
+  end
+  .join("\n")
+
+  Event.create(
+    organizer: u,
+    title: Faker::Hacker.say_something_smart,
+    description: description,
+    start_time: Faker::Date.forward(10),
+    end_time: Faker::Date.between(10.days.from_now, 15.days.from_now),
+    spots: rand(5,15)
+  )
+end
