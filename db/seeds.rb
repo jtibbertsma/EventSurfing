83373a4586c4b86173ea15332b98316d6dddcf64
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-08-13 15:54:08 -0700

    More seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,20 +13,58 @@ def make_description
   .join("\n")
 end
 
-u = User.create(
+test_user = User.create(
   name: "Joseph Tibbertsma",
   email: "email@email.com",
   password: "password123",
   description_head: make_description
 )
 
-15.times do
+# Create 3 events owned by test user
+3.times do
   Event.create(
-    organizer: u,
-    title: Faker::Commerce.product_name,
+    organizer: test_user,
+    title: Faker::Company.bs,
     description: make_description,
     start_time: Faker::Date.forward(10),
     end_time: Faker::Date.between(10.days.from_now, 15.days.from_now),
     spots: rand(5...15)
   )
+end
+
+# Create 30 more users
+other_users = 30.times.inject([]) do |memo, _|
+  memo << User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    description_head: make_description
+  )
+end
+
+# Create 15 events owned by other users
+15.times do
+  Event.create(
+    organizer: other_users.sample,
+    title: Faker::Company.bs,
+    description: make_description,
+    start_time: Faker::Date.forward(10),
+    end_time: Faker::Date.between(10.days.from_now, 15.days.from_now),
+    spots: rand(5...15)
+  )
+end
+
+# Have each user join 3 events
+events = Event.all.to_a
+User.all.each do |user|
+  3.times do
+    # Ignore attemps to create duplicate event joins
+    begin
+      EventJoin.create(
+        attender: user,
+        event: events.sample
+      )
+    rescue ActiveRecord::RecordNotUnique
+    end
+  end
 end
