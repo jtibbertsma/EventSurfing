aaeced3e4541408ca6ea97fdfca428891adebeb5
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-08-14 11:15:34 -0700

    Threw together some views for attender list on event show page

---
+++ app/assets/javascripts/views/users/attender_list_item.js
@@ -0,0 +1,13 @@
+PadCrashing.Views.AttenderIndexItem = Backbone.View.extend({
+  template: JST["users/attender_list_item"],
+  className: "attender-list-item",
+
+  initialize: function () {
+    this.listenTo(this.model, "sync", this.render);
+  },
+
+  render: function () {
+    this.$el.html(this.template({ attender: this.model }));
+    return this;
+  }
+});
--- app/assets/javascripts/views/users/event_attenders.js
+++ app/assets/javascripts/views/users/event_attenders.js
@@ -1,5 +1,21 @@
 PadCrashing.Views.EventAttenders = Backbone.CompositeView.extend({
+  template: JST["users/event_attenders"],
+
+  initialize: function () {
+    this.listenTo(this.collection, "sync add remove", this.render);
+    this.listenTo(this.collection, "add", this.addAttender);
+  },
+
+  addAttender: function (attender) {
+    this.addSubview(
+      ".attender-holder",
+      new PadCrashing.Views.AttenderListItem({ model: attender })
+    );
+  },
+
   render: function () {
-    
+    this.$el.html(this.template());
+    this.attachSubviews();
+    return this;
   }
 });
