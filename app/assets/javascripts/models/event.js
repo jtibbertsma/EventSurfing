5c8e0269f1f2cbf516f744897aa65736e933318d
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-08-14 11:50:00 -0700

    Parse event attenders and refactor

--- app/assets/javascripts/models/event.js
+++ app/assets/javascripts/models/event.js
@@ -1,7 +1,15 @@
 PadCrashing.Models.Event = Backbone.Model.extend({
   urlRoot: "api/events",
 
-  parse: function (payload) {
+  _parseAttenders: function (payload) {
+    if (payload.attenders) {
+      this.attenders = new PadCrashing.Models.Users();
+      this.attenders.set(payload.attenders);
+      delete payload.attenders;
+    }
+  },
+
+  _parseTimes: function (payload) {
     payload.start_time = moment(
       payload.start_time
     ).format('MMMM Do YYYY, h:mm a');
@@ -11,11 +19,19 @@ PadCrashing.Models.Event = Backbone.Model.extend({
         payload.end_time
       ).format('MMMM Do YYYY, h:mm a');
     }
+  },
 
+  _parseJoin: function (payload) {
     if (payload.join) {
       this.join = new PadCrashing.Models.EventJoin(payload.join);
       delete payload.join
     }
+  },
+
+  parse: function (payload) {
+    this._parseAttenders(payload);
+    this._parseTimes(payload);
+    this._parseJoin(payload);
 
     if (payload.spots_remaining !== 0 && !payload.spots_remaining) {
       payload.spots_remaining = -1;
