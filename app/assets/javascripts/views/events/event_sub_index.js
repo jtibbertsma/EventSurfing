2198dc3757210f5745dde1cd129cb74dfdc60a76
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-08-14 14:26:06 -0700

    Added index and template to render user specific events

---
+++ app/assets/javascripts/views/events/event_sub_index.js
@@ -0,0 +1,25 @@
+// The difference between this and the main index is that this doesn't generate
+// join/unjoin button along with the index item text
+
+PadCrashing.Views.EventSubIndex = Backbone.CompositeView.extend({
+  template: JST["events/sub_index"],
+
+  initialize: function () {
+    this.collection.each(this.addIndexItemText.bind(this));
+    this.listenTo(this.collection, "add remove sync", this.render);
+    this.listenTo(this.collection, "add", this.addIndexItemText);
+  },
+
+  addIndexItemText: function (event) {
+    this.addSubview(
+      ".index-item-holder",
+      new EventIndexItemText({ model: event })
+    );
+  },
+
+  render: function () {
+    this.$el.html(this.template());
+    this.attachSubviews();
+    return this;
+  }
+});
