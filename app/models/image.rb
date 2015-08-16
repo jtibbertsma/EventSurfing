ea86a0a858cea48ed10fb9427746f48b405ffd36
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-08-16 02:57:44 -0700

    Ensure that an event or user has only one image

--- app/models/image.rb
+++ app/models/image.rb
@@ -15,4 +15,17 @@ class Image < ActiveRecord::Base
   belong_to :imageable, polymorphic: true
 
   validates :imageable, :image_url, :thumb_url, presence: true
+
+  before_save :ensure_unique
+
+  private
+
+  def ensure_unique
+    old_image = Image.find_by(
+      imageable_id: imageable_id,
+      imageable_type: imageable_type
+    )
+
+    old_image && old_image.destroy()
+  end
 end
