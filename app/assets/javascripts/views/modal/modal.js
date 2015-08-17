5b9c3e977a0a4f047ec7a175defb29ea53a8dc0f
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-08-17 11:29:29 -0700

    Event form zombies have been purged

--- app/assets/javascripts/views/modal/modal.js
+++ app/assets/javascripts/views/modal/modal.js
@@ -2,15 +2,13 @@ PadCrashing.Views.Modal = Backbone.View.extend({
   template: JST["modal/modal"],
 
   initialize: function (options) {
-    this.navigateBack = options.navigateBack;
-
     this.subview = options.subview;
     this.subview.hideModal = function () {
       this.$("#myModal").modal("hide");
     }.bind(this)
 
-    this.$(".modal").on("hidden.bs.modal", this.remove.bind(this));
     this.render();
+    this.$(".modal").on("hidden.bs.modal", this.remove.bind(this));
   },
 
   render: function () {
@@ -27,9 +25,10 @@ PadCrashing.Views.Modal = Backbone.View.extend({
   },
 
   remove: function () {
-    if (this.navigateBack) {
-      Backbone.history.navigate(this.navigateBack, { trigger: false });
-    }
+    // kill all cloudinary iframes
+    $("iframe").remove();
+
+    // kill listener
     this.$(".modal").off("hidden.bs.modal", this.remove.bind(this));
     this.subview.remove();
     Backbone.View.prototype.remove.call(this);
