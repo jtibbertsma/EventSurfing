3dec8045f4e7beb802dc9a9dce4d373febdb9814
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-08-24 22:24:33 -0700

    Fix minor bug

--- app/assets/javascripts/views/users/user_show_about.js
+++ app/assets/javascripts/views/users/user_show_about.js
@@ -1,6 +1,10 @@
 EventSurfing.Views.UserShowAbout = Backbone.View.extend({
   template: JST["users/show_about"],
 
+  initialize: function () {
+    this.listenTo(this.model, "sync", this.render);
+  },
+
   render: function () {
     this.$el.html(this.template({ user: this.model }));
     return this;
